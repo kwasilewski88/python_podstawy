cc19199caa37265829f87d96b88d18c2fee213a8
kwasilewski88 <kwasilewski88@github.com>
2023-05-12 14:47:25 +0200

    9.tasks 3,5

--- 9. Podstawy Pętle Rozszerzenie/zad 3.py
+++ 9. Podstawy Pętle Rozszerzenie/zad 3.py
@@ -1,6 +1,6 @@
-LUDZIE  = ['Adam', 'Stanisław', 'Joanna', 'Kornelia', 'Kacper']
+PEOPLE = ['Adam', 'Stanisław', 'Joanna', 'Kornelia', 'Kacper']
 
-for i in range(len(LUDZIE)):
-    for j in range(len(LUDZIE)):
+for i in range(len(PEOPLE)):
+    for j in range(len(PEOPLE)):
         if j > i:
-            print(f"polaczenie {LUDZIE[i]}  z  {LUDZIE[i]}")
+            print(f"polaczenie {PEOPLE[i]}  z  {PEOPLE[i]}")
--- 9. Podstawy Pętle Rozszerzenie/zad 5.py
+++ 9. Podstawy Pętle Rozszerzenie/zad 5.py
@@ -1,8 +1,7 @@
-zamowienia = {"Klient_1335": {"nazwa_potrawy": "rosół", "ocena": 5, "rachunek": 20.0},
+orders = {"Klient_1335": {"nazwa_potrawy": "rosół", "ocena": 5, "rachunek": 20.0},
               "Klient_222": {"nazwa_deseru": "lody waniliowe", "rachunek": 5.0}}
 
-
-for key, value in zamowienia.items():
+for key, value in orders.items():
     print(key)
     for key_, value_ in value.items():
         print(key_, value_)
